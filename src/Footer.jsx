dca2e1d4da64db61bc99660105be886d0c0f4c4a
Kushagra Anand <kushrokz95@gmail.com>
2020-09-22 20:10:28 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ textAlign: "center", color: "black" }}>
+      Made With ❤️ by Kushagra |{" "}
+      <a
+        style={{ textDecoration: "none" }}
+        target="_blank"
+        href="https://myportfolio-kush.netlify.app/"
+      >
+        Contact Developer
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
