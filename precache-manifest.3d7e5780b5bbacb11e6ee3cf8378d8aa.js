8a3adbc507f4ba3366ce35697d082dcc7f54c802
Kushagra Anand <kushrokz95@gmail.com>
2020-09-22 20:15:15 +0530

    Updates

---
+++ precache-manifest.3d7e5780b5bbacb11e6ee3cf8378d8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7028036b2000d37da13aed46f88892",
+    "url": "/covid-19-flexbox/index.html"
+  },
+  {
+    "revision": "6c68bec402d1356cf613",
+    "url": "/covid-19-flexbox/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "18e373c8937d72592fac",
+    "url": "/covid-19-flexbox/static/css/main.51c2042d.chunk.css"
+  },
+  {
+    "revision": "6c68bec402d1356cf613",
+    "url": "/covid-19-flexbox/static/js/2.1d5bb5f4.chunk.js"
+  },
+  {
+    "revision": "6788bbbeb96ac5b9fb2d2231815fe398",
+    "url": "/covid-19-flexbox/static/js/2.1d5bb5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e373c8937d72592fac",
+    "url": "/covid-19-flexbox/static/js/main.1f8f3c2d.chunk.js"
+  },
+  {
+    "revision": "3cc4cc7c5e01edc94928",
+    "url": "/covid-19-flexbox/static/js/runtime-main.63f973b0.js"
+  }
+]);
